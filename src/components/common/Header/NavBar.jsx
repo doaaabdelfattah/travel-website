3ce7ce77b53ea76f869eeb3b191f2533555cf605
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-10-21 13:51:30 +0300

    adjust booking

--- src/components/common/Header/NavBar.jsx
+++ src/components/common/Header/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
 
   return (
     <div
-      className={`fixed h-[150px] w-full border-b-[1.5px] border-white/[0.2] flex justify-between items-center p-[50px] z-50
+      className={`fixed h-[150px] w-full border-b-[1.5px] border-white/[0.2] flex justify-between items-center p-[50px] z-[99]
     ${
       isScrolled
         ? "bg-gray-200/50 backdrop-blur-md shadow-md"
